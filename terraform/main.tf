f57696b62cb37de13b4e50cae20b03805c900838
Oleg Skogorev <oskogorev@gmail.com>
2020-10-04 22:37:14 +0400

    Preparing fifes for HW before stars

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,12 +1,11 @@
 provider "yandex" {
-token     = "AgAAAABCS__nAATuwfzImpKBEUtwjBgIzE-7Kww"
-cloud_id  = "b1gf6ugjqc3frqm0ijqb"
-folder_id = "b1g4l494cgnejfisqgv2"
-zone      = "ru-central1-a"
+  service_account_key_file = var.service_account_key_file
+  cloud_id                 = var.cloud_id
+  folder_id                = var.folder_id
 }
 resource "yandex_compute_instance" "app" {
   name = "reddit-app"
-
+  zone = var.zone
   resources {
     cores  = 2
     memory = 2
@@ -15,34 +14,34 @@ resource "yandex_compute_instance" "app" {
   boot_disk {
     initialize_params {
       # Указать id образа созданного в предыдущем домашем задании
-      image_id = "fd8oqoi914e13ecv3987"
+      image_id = var.image_id
     }
   }
 
   network_interface {
     # Указан id подсети default-ru-central1-a
-    subnet_id = "e9bq8eg9210chldu8unl"
+    subnet_id = var.subnet_id
     nat       = true
   }
 
   metadata = {
-      ssh-keys = "ubuntu:${file("~/.ssh/appuser.pub")}"
+    ssh-keys = "ubuntu:${file(var.public_key_path)}"
   }
 
   connection {
-  type = "ssh"
-  host = yandex_compute_instance.app.network_interface.0.nat_ip_address
-  user = "ubuntu"
-  agent = false
-  # путь до приватного ключа
-  private_key = file("~/.ssh/appuser")
+    type  = "ssh"
+    host  = yandex_compute_instance.app.network_interface.0.nat_ip_address
+    user  = "ubuntu"
+    agent = false
+    # путь до приватного ключа
+    private_key = file(var.private_key_path)
   }
 
   provisioner "file" {
-  source = "files/puma.service"
-  destination = "/tmp/puma.service"
-}
+    source      = "files/puma.service"
+    destination = "/tmp/puma.service"
+  }
   provisioner "remote-exec" {
-  script = "files/deploy.sh"
-}
+    script = "files/deploy.sh"
+  }
 }
