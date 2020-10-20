e499e40c985891f1fdc92f6f5659281a1a42a4bb
Oleg Skogorev <oskogorev@gmail.com>
2020-10-21 00:30:52 +0400

    Second try to create vpc module

--- terraform/modules/db/main.tf
+++ terraform/modules/db/main.tf
@@ -16,7 +16,7 @@ resource "yandex_compute_instance" "db" {
   }
 
   network_interface {
-    subnet_id = var.subnet_id
+    subnet_id = value = yandex_vpc_subnet.app-subnet.id
     nat       = true
   }
 
---
+++ terraform/modules/vpc/main.tf
@@ -0,0 +1,15 @@
+resource "yandex_vpc_network" "app-network" {
+  name = "reddit-app-network"
+  labels = {
+    tags = "reddit-app-network"
+  }
+}
+resource "yandex_vpc_subnet" "app-subnet" {
+  name = "reddit-app-subnet"
+  labels = {
+    tags = "reddit-app-subnet"
+  }
+  zone           = var.zone
+  network_id     = "${yandex_vpc_network.app-network.id}"
+  v4_cidr_blocks = ["192.168.10.0/24"]
+}
